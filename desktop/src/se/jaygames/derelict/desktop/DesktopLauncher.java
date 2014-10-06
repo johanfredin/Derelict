bfa3c78e092963a8009a8d04d260d202fcc52a32
Johan Fredin <johan.fredin@retroidinteractive.com>
2014-10-06 20:44:12 +0200

    First commit

---
+++ desktop/src/se/jaygames/derelict/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.jaygames.derelict.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.jaygames.derelict.Derelict;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Derelict(), config);
+	}
+}
