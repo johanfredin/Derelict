280d70561e08d44fe3f1bbc85deee5248160f17e
Johan Fredin <johan.fredin@retroidinteractive.com>
2014-10-14 21:51:23 +0200

    Added more functionality from GDXTensions for testing

--- core/src/se/jaygames/derelict/object/Player.java
+++ core/src/se/jaygames/derelict/object/Player.java
@@ -4,8 +4,10 @@ import se.fredin.gdxtensions.assetmanagement.Assets;
 import se.fredin.gdxtensions.input.GeneralInput;
 import se.fredin.gdxtensions.input.InputDevice;
 import se.fredin.gdxtensions.object.GameObject;
+import se.fredin.gdxtensions.utils.AnimationUtils;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -14,6 +16,10 @@ public class Player extends GameObject {
 
 	private TextureRegion currentFrame;
 	private final byte DEFAULT_SPEED = 4;
+	private Animation leftAnimation;
+	private Animation rightAnimation;
+	private Animation upAnimation;
+	private Animation downAnimation;
 	
 	//TODO: For now only fixed keyboard, fix later to map to any keys or gamepad or touchpad
 	private GeneralInput input;
@@ -22,13 +28,19 @@ public class Player extends GameObject {
 		super(position);
 		this.speed = DEFAULT_SPEED;
 		this.input = new GeneralInput(InputDevice.KEYBOARD);
-		this.currentFrame = new TextureRegion((Texture) Assets.getInstance().get("sprites/objects/test/hero.png"), 0, 0, 32, 32);
+		TextureRegion heroSpriteSheet = new TextureRegion((Texture) Assets.getInstance().get("sprites/objects/test/hero.png"));
+		this.leftAnimation = AnimationUtils.getAnimation(heroSpriteSheet, .10f, 32, 32, "0-11");
+		this.rightAnimation = AnimationUtils.getAnimation(heroSpriteSheet, .10f, 32, 32, false, false, 6,7,8);
+		this.upAnimation = AnimationUtils.getAnimation(heroSpriteSheet, .10f, 32, 32, false, false, 9,10,11);
+		this.downAnimation = AnimationUtils.getAnimation(heroSpriteSheet, .10f, 32, 32, false, false, "0-2");
+		this.currentFrame = rightAnimation.getKeyFrame(stateTime, false);
 	}
 
 	@Override
 	public void tick(float deltaTime, GameObject gameObject) {
 		position.add(velocity);
 		bounds.setPosition(position);
+		animate(deltaTime);
 		handleInput();
 	}
 	
@@ -36,21 +48,42 @@ public class Player extends GameObject {
 	public void render(SpriteBatch batch) {
 		batch.draw(currentFrame, position.x, position.y);
 	}
+	
+	@Override
+	protected void animate(float deltaTime) {
+		super.animate(deltaTime);
+		if(isHeadingUp()) {
+			currentFrame = upAnimation.getKeyFrame(stateTime, true);
+		} else if(isHeadingDown()) {
+			currentFrame = downAnimation.getKeyFrame(stateTime, true);
+		} else if(isHeadingRight()) {
+			currentFrame = rightAnimation.getKeyFrame(stateTime, true);
+		} else if(isHeadingLeft()) {
+			currentFrame = leftAnimation.getKeyFrame(stateTime, true);
+		} else {
+			currentFrame = downAnimation.getKeyFrame(0.0f);
+		}
+	}
 
 	@Override
 	public void dispose() {}
 
 	private void handleInput() {
-		if(input.upButtonPressed) {
+		if(input.isUpButtonPressed()) {
 			velocity.set(0, speed);
-		} else if(input.downButtonPressed) {
+			setDirection(DIRECTION_UP);
+		} else if(input.isDownButtonPressed()) {
 			velocity.set(0, -speed);
-		} else if(input.leftButtonPressed) {
+			setDirection(DIRECTION_DOWN);
+		} else if(input.isLeftButtonPressed()) {
 			velocity.set(-speed, 0);
-		} else if(input.rightButtonPressed) {
+			setDirection(DIRECTION_LEFT);
+		} else if(input.isRightButtonPressed()) {
 			velocity.set(speed, 0);
+			setDirection(DIRECTION_RIGHT);
 		} else {
 			velocity.set(0, 0);
+			setDirection(DIRECTION_NONE);
 		}
 	}
 
--- desktop/src/se/jaygames/derelict/desktop/DesktopLauncher.java
+++ desktop/src/se/jaygames/derelict/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import se.jaygames.derelict.Derelict;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
+		config.width = 600;
+		config.height = 400;
 		new LwjglApplication(new Derelict(), config);
 	}
 }
